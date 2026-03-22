92149dfba499a28c1a2b79053e08a0be18b8616f
uditgarg007 <gargudit69@gmail.com>
2026-03-23 00:13:52 +0530

    Update temp.cpp

--- temp.cpp
+++ temp.cpp
@@ -2,21 +2,4 @@
 using namespace std;
 
 
-int main()
-{
-    int t;
-    cin>>t;
-    while(t--)
-    {
-        int n;
-        cin>>n;
-        vector<int> v(n);
-        for(int i=0;i<n;i++)
-            cin>>v[i];
-        sort(v.begin(),v.end());
-        int ans=0;
-        for(int i=0;i<n-1;i++)
-            ans+=v[i+1]-v[i];
-        cout<<ans<<endl;
-    }
-}
+int 
